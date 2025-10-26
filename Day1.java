9e79b644e7bf45934a158e3526ea3c738015226a
sanika1971 <sanikagaikwad189@gmail.com>
2025-10-26 20:32:36 +0530

    Day1 palindrome

---
+++ Day1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Day1 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int n = x;
+        int revNum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revNum = revNum * 10 + d;
+            n = n / 10;
+        }
+
+        return revNum == x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
