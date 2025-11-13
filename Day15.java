1d05cbd6d6e1b069797e3cbbc642cd0a462bdd10
sanika1971 <sanikagaikwad189@gmail.com>
2025-11-13 22:26:20 +0530

    Add Day15 bulbSwitch Java program

---
+++ Day15.java
@@ -0,0 +1,21 @@
+public class Day15 {
+    static class Solution {
+        public int bulbSwitch(int n) {
+            int count = 0;
+            int i = 1;
+
+            while (i * i <= n) {
+                count++;
+                i++;
+            }
+
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 10;
+        System.out.println(sol.bulbSwitch(n)); // Output: 3
+    }
+}
