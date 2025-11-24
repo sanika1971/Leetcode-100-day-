353646e8074ecb923ed869a539e235a61b5cb646
sanika1971 <sanikagaikwad189@gmail.com>
2025-11-24 23:26:36 +0530

    Add binary search solution in Day20 program

---
+++ Day21.java
@@ -0,0 +1,31 @@
+public class Day21 {
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        int result = search(nums, target);
+
+        System.out.println("Index of target: " + result);
+    }
+}
+
+
